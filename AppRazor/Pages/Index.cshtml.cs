b276106a71924a40ab82051c6ee69679df16f189
christshan26 <christoffersten.hansson@gmail.com>
2025-12-01 20:37:10 +0100

    experimenting with Index.cshtml

--- AppRazor/Pages/Index.cshtml.cs
+++ AppRazor/Pages/Index.cshtml.cs
@@ -1,19 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models.Interfaces;
+using Services.Interfaces;
+
 
 namespace AppRazor.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+        private readonly IAddressesService _addressesService;
+        private readonly IAdminService _adminService;
+        public IEnumerable<Models.DTO.GstUsrInfoFriendsDto>? CountryInfo;
+        
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+        public async Task<IActionResult> OnGet()
+        {
+            var addresses = await _addressesService.ReadAddressesAsync(true, false, "Denmark", 0, 10);
+            var info = await _adminService.GuestInfoAsync();
 
-    public void OnGet()
-    {
+            CountryInfo = info.Item.Friends.Where(f => f.Country == "Denmark");
+            return Page();
+        }
 
-    }
+        public IndexModel(IAddressesService addressesService, IAdminService adminService)
+        {
+            _addressesService = addressesService;
+            _adminService = adminService;
+        }
 }
