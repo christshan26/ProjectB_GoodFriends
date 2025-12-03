434f402e3f19215ce3c50eedc433012e70c3a5f8
christshan26 <christoffersten.hansson@gmail.com>
2025-12-03 21:45:20 +0100

    Overview on Friends per Country done

--- AppRazor/Pages/Friends/Overview.cshtml.cs
+++ AppRazor/Pages/Friends/Overview.cshtml.cs
@@ -7,22 +7,21 @@ namespace AppRazor.Pages.Friends
 {
     public class OverviewModel : PageModel
     {
-        private readonly IAddressesService _addressesService;
         private readonly IAdminService _adminService;
         public IEnumerable<Models.DTO.GstUsrInfoFriendsDto>? CountryInfo;
 
         public async Task<IActionResult> OnGet()
         {
-            var addresses = await _addressesService.ReadAddressesAsync(true, false, "Denmark", 0, 10);
             var info = await _adminService.GuestInfoAsync();
 
-            CountryInfo = info.Item.Friends.Where(f => f.Country == "Denmark");
+            CountryInfo = info.Item.Friends
+                .Where(f => f.City == null && f.Country != null);
+
             return Page();
         }
 
-        public OverviewModel(IAddressesService addressesService, IAdminService adminService)
+        public OverviewModel(IAdminService adminService)
         {
-            _addressesService = addressesService;
             _adminService = adminService;
         }
     }
