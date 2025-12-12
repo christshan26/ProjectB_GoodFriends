b8112c503f8cb362a636080e0cfe2eb00b3e8784
christshan26 <christoffersten.hansson@gmail.com>
2025-12-12 09:49:48 +0100

    Started implementing EditFriend

--- AppRazor/Pages/Friends/Detailview/EditFriend.cshtml.cs
+++ AppRazor/Pages/Friends/Detailview/EditFriend.cshtml.cs
@@ -1,12 +1,36 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Models.Interfaces;
+using Models.DTO;
+using Services;
+using Services.Interfaces;
+using AppRazor.SeidoHelpers;
+using Newtonsoft.Json;
 
 namespace AppRazor.Pages.Friends.Detailview
 {
     public class EditFriendModel : PageModel
     {
-        public void OnGet()
+        private readonly IFriendsService _friendsService = null;
+        private readonly IAddressesService _addressesService = null;
+        public IFriend Friend { get; set; }
+        public IAddress Address { get; set; }
+
+        public async Task<IActionResult> OnGet()
         {
+            Guid _friendId = Guid.Parse(Request.Query["id"]);
+            Friend = (await _friendsService.ReadFriendAsync(_friendId, false)).Item;
+
+            return Page();
+        }
+    
+
+        public EditFriendModel(IFriendsService friendsService, IAddressesService addressesService)
+        {
+            _friendsService = friendsService;
+            _addressesService = addressesService;
         }
     }
 }
